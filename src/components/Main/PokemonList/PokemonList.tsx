1a515be513a9501b2f13f40a6f2fbd1795fbcb35
Michail <astapenko.m99@gmail.com>
2025-07-24 16:30:30 +0300

    refactor: rewrite Loader and PokemonList components from class to functional

--- src/components/Main/PokemonList/PokemonList.tsx
+++ src/components/Main/PokemonList/PokemonList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './PokemonList.css';
 import Button from '../../../utils/Button';
 import type { Pokemon } from '../../../utils/interfaces';
@@ -14,38 +13,32 @@ type PokemonListProps = {
   handleNext: () => void;
 };
 
-class PokemonList extends React.Component<PokemonListProps> {
-  render() {
-    const { pokemons, handlePrevious, handleNext } = this.props;
-    return (
-      <div>
-        <h1>Pokémon List</h1>
-        <div className="list-item">
-          {pokemons.map((pokemon) => (
-            <div key={pokemon.id} className="pokemon-item">
-              <img
-                src={pokemon.img}
-                alt={pokemon.name}
-                className="pokemon-img"
-              />
-              <h4 className="pokemon-name">{pokemon.name}</h4>
-              <p className="pokemon-types">Type: {pokemon.types}</p>
-              <p className="pokemon-types">Experience: {pokemon.experience}</p>
-            </div>
-          ))}
-        </div>
-
-        <div className="buttons-container">
-          <Button onClick={handlePrevious} disabled={this.props.prev === null}>
-            Prev
-          </Button>
-          <Button onClick={handleNext} disabled={this.props.next === null}>
-            Next
-          </Button>
-        </div>
+const PokemonList = (props: PokemonListProps) => {
+  const { pokemons, handlePrevious, handleNext } = props;
+  return (
+    <div>
+      <h1>Pokémon List</h1>
+      <div className="list-item">
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.id} className="pokemon-item">
+            <img src={pokemon.img} alt={pokemon.name} className="pokemon-img" />
+            <h4 className="pokemon-name">{pokemon.name}</h4>
+            <p className="pokemon-types">Type: {pokemon.types}</p>
+            <p className="pokemon-types">Experience: {pokemon.experience}</p>
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+
+      <div className="buttons-container">
+        <Button onClick={handlePrevious} disabled={props.prev === null}>
+          Prev
+        </Button>
+        <Button onClick={handleNext} disabled={props.next === null}>
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default PokemonList;
